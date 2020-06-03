ccaf7a3213f2227c3dadc1191b2a94f101ef2027
Neissa Dorsinville <neissa@dorsinville.com>
2020-06-03 17:05:05 -0400

    Folder Home coompleted, close issue #6, close issue #7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  {Component} from 'react';
+import ToggleFolder from"./ToggleFolder.js"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+      <ToggleFolder>
+        <ul>
+        <li> File 1 </li>
+        <li> File 2 </li>
+        <li> File 3 </li>
+        </ul>
+      </ToggleFolder>
+      </>
+    );
+  }
 }
 
 export default App;
